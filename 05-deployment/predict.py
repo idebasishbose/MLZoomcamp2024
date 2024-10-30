c312c4926ae24cb1ca7d98e45db5e16ff7e6d73e
Debasish Bose <devbose2008@gmail.com>
2024-10-30 10:37:54 -0400

    week 5 mid commit

--- 05-deployment/predict.py
+++ 05-deployment/predict.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
 
-
+app = Flask("churn")
 input_file = "model_C=1.0.bin"
 
 
@@ -8,36 +14,18 @@ with open(input_file, "rb") as f_in:
     (dv, model) = pickle.load(f_in)
 
 
-dv, model
+@app.route("/predict", methods=["POST"])
+def predict():
+    customer = request.get_json()
+
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred > 0.5
+
+    result = {"churn_probability": float(y_pred), "churn": bool(churn)}
+
+    return jsonify(result)
 
 
-customer = {
-    "gender": "female",
-    "seniorcitizen": 0,
-    "partner": "yes",
-    "dependents": "no",
-    "phoneservice": "no",
-    "multiplelines": "no_phone_service",
-    "internetservice": "dsl",
-    "onlinesecurity": "no",
-    "onlinebackup": "yes",
-    "deviceprotection": "no",
-    "techsupport": "no",
-    "streamingtv": "no",
-    "streamingmovies": "no",
-    "contract": "month-to-month",
-    "paperlessbilling": "yes",
-    "paymentmethod": "electronic_check",
-    "tenure": 1,
-    "monthlycharges": 29.85,
-    "totalcharges": 29.85,
-}
-
-
-X = dv.transform([customer])
-
-
-y_pred = model.predict_proba(X)[0, 1]
-
-print("input", customer)
-print("churn prediction", y_pred)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=9696)
